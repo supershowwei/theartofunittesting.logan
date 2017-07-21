e05554bc8896e669554b741c4928de44ef5f4af6
johnny <johnny@wantgoo.com>
2017-07-21 10:36:16 +0800

    page 22 finish

---
+++ LogAn/LogAnalyzer.cs
@@ -0,0 +1,15 @@
+﻿namespace LogAn
+{
+    public class LogAnalyzer
+    {
+        public bool IsValidLogFileName(string fileName)
+        {
+            if (fileName.EndsWith(".SLF"))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
