17fd7fe1fb8afc953b25029042503c72ba52dbc4
johnny <johnny@wantgoo.com>
2017-07-21 10:39:08 +0800

    page 23 finish

---
+++ LogAn.UnitTests/LogAnalyzerTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LogAn.UnitTests
+{
+    [TestClass]
+    public class LogAnalyzerTests
+    {
+        [TestMethod]
+        public void IsValidLogFileName_BadExtension_ReturnFalse()
+        {
+        }
+    }
+}
