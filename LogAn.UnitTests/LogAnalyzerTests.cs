535d81083476f9ea10f3d96281d74f8869f67b5b
johnny <johnny@wantgoo.com>
2017-07-21 11:00:45 +0800

    page 27~28 finish

--- LogAn.UnitTests/LogAnalyzerTests.cs
+++ LogAn.UnitTests/LogAnalyzerTests.cs
@@ -15,5 +15,15 @@ namespace LogAn.UnitTests
 
             Assert.IsFalse(actual);
         }
+
+        [TestMethod]
+        public void IsValidLogFileName_GoodExtensionLowercase_ReturnsTrue()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+
+            bool actual = analyzer.IsValidLogFileName("filewithgoodextension.slf");
+
+            Assert.IsTrue(actual);
+        }
     }
 }
